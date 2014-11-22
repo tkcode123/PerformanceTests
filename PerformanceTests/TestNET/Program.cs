444c1f01b64a179e6dd625fe95eae0e5e97b97ee
tkcode123 <tkcode123@gmail.com>
2014-11-22 22:57:51 +0100

    * Program.cs: Simple Beginning
    * AssemblyInfo.cs:

---
+++ PerformanceTests/TestNET/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TestNET
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Performance");
+			var start = DateTime.UtcNow;
+			int h = 0;
+			for (int i = 0; i < 10000000; i++)
+				h += i.ToString ("x8").Length;
+
+			var end = DateTime.UtcNow;
+			Console.WriteLine ("{1} Took {0}", (end-start), h);
+		}
+	}
+}
